1ccd3cefc14f337bb6eecf28cd5476da77df97f5
anoopchaurasia <anoop26667@gmail.com>
2014-04-08 17:22:05 +0530

    added tes cases

--- test.js
+++ test.js
@@ -4,18 +4,35 @@ setTimeout(function(){
  console.log(fm.getMissingClass());
 }, 1000);
 global.GlobalStringMap = {doctor: "Doctor"};
+global. GlobalSession = {};
+global.$ = {
+	each:function(arr, cb){
+		arr.forEach(function(data, index){
+			cb.call(data, index);
+		});
+	}
+};
  _id = 12;
  "use strict"
  require("jsfm");
  fm.Include("F:/practo/Public_html/js/vendor/dateformat.js");
-var data = require('fs').readFileSync("./" + process.argv[2]).toString("utf-8");
-data = JSON.parse(data);
-
-fm.packages = data.sourceDirs || data.sourceDirs;
+ fm.Include("F:/practo/Public_html/js/vendor/date.js");
+ fm.Include("F:/practo/Public_html/js/vendor/parser.js");
+var testCeses = require('fs').readFileSync("./" + process.argv[2]).toString("utf-8");
+testCeses = JSON.parse(testCeses);
+fm.packages = testCeses.sourceDirs || testCeses.sourceDirs;
+fm.Include("records.Utility");
+fm.Include("calendar.serverCompatible");
+fm.Include("calendar.Server");
 fm.Include("records.Settings");
- fm.Include(data.class, function() {
-	console.log(data.methods);
-	test(data);
+
+fm.Include("records.DateClass");
+ fm.Include(testCeses.class, function() {
+	var data = require("fs").readFileSync("./data/settings.json").toString("utf-8");
+	data = JSON.parse(data);
+	DoctorList.set(data.doctors);
+	Settings.set(data);
+	test(testCeses);
  });
  
 
@@ -25,8 +42,17 @@ fm.Include("records.Settings");
 	console.log("method", "parameters", "expected", "result", "status");
     fm.Include(cases.class, function(){
 		var cls = fm.stringToObject(cases.class);
-		var instance = new cls(), passedCount = 0, failedCount = 0;
-		
+		var argv = cases.args;
+		var instance = new cls(argv[0], argv[1], argv[2], argv[3], argv[4], argv[5]), passedCount = 0, failedCount = 0;
+		for(var key in cases.instance){
+		   if( cases.instance[key] === instance[key]){
+				passedCount++;
+				//console.log(method.method, test.args, test.expected, result, "Pass");
+			}else{
+				failedCount++;
+				console.error(key, "prop", instance[key], cases.instance[key], "Fail");
+			}
+		}
 		cases.methods.forEach(function(method){
 			fn = null;
 			if( method.methodType === 'static'){
