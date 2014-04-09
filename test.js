040b832c6533fc9f457b389f3ca17d431dfe1190
anoopchaurasia <anoop26667@gmail.com>
2014-04-09 11:28:27 +0530

    added patient object

--- test.js
+++ test.js
@@ -5,34 +5,98 @@ setTimeout(function(){
 }, 1000);
 global.GlobalStringMap = {doctor: "Doctor"};
 global. GlobalSession = {};
-global.$ = {
-	each:function(arr, cb){
-		arr.forEach(function(data, index){
-			cb.call(data, index);
-		});
-	}
+global.document = {};
+global.$ = function(){
+return {bind:function(){}};
 };
+function isArraylike( obj ) {
+	var length = obj.length;
+
+	if ( obj.nodeType === 1 && length ) {
+		return true;
+	}
+
+	return ( length === 0 ||
+		typeof length === "number" && length > 0 && ( length - 1 ) in obj );
+}
+$.each = function( obj, callback, args ) {
+		var value,
+			i = 0,
+			length = obj.length,
+			isArray = isArraylike( obj );
+
+		if ( args ) {
+			if ( isArray ) {
+				for ( ; i < length; i++ ) {
+					value = callback.apply( obj[ i ], args );
+
+					if ( value === false ) {
+						break;
+					}
+				}
+			} else {
+				for ( i in obj ) {
+					value = callback.apply( obj[ i ], args );
+
+					if ( value === false ) {
+						break;
+					}
+				}
+			}
+
+		// A special, fast, case for the most common use of each
+		} else {
+			if ( isArray ) {
+				for ( ; i < length; i++ ) {
+					value = callback.call( obj[ i ], i, obj[ i ] );
+
+					if ( value === false ) {
+						break;
+					}
+				}
+			} else {
+				for ( i in obj ) {
+					value = callback.call( obj[ i ], i, obj[ i ] );
+
+					if ( value === false ) {
+						break;
+					}
+				}
+			}
+		}
+
+		return obj;
+	};
+
  _id = 12;
  "use strict"
  require("jsfm");
  fm.Include("F:/practo/Public_html/js/vendor/dateformat.js");
  fm.Include("F:/practo/Public_html/js/vendor/date.js");
  fm.Include("F:/practo/Public_html/js/vendor/parser.js");
+ 
 var testCeses = require('fs').readFileSync("./" + process.argv[2]).toString("utf-8");
 testCeses = JSON.parse(testCeses);
-fm.packages = testCeses.sourceDirs || testCeses.sourceDirs;
+fm.packages = testCeses.sourceDirs || testCeses.packages;
+console.log(fm.packages);
 fm.Include("records.Utility");
+fm.Include("records.Settings");
+fm.Include("records.patient.Patient");
 fm.Include("calendar.serverCompatible");
 fm.Include("calendar.Server");
-fm.Include("records.Settings");
 
 fm.Include("records.DateClass");
  fm.Include(testCeses.class, function() {
+	setTimeout(function(){
 	var data = require("fs").readFileSync("./data/settings.json").toString("utf-8");
 	data = JSON.parse(data);
-	DoctorList.set(data.doctors);
 	Settings.set(data);
+	DoctorList.set(data.doctors);
+	data = require("fs").readFileSync("./data/patient.json").toString("utf-8")
+	data = JSON.parse(data);
+	new records.patient.Patient(data);
 	test(testCeses);
+	}, 1000);
  });
  
 
